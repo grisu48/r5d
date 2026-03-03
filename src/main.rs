d0b419225ddbc94d58bfc6f5e079c182be159efd
phoenix <felix.niederwanger@suse.com>
2026-03-03 19:39:29 +0100

    Add usage
    Add argument parsing routine for future usage and add usage messages via
    -h or --help.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,25 @@ use std::{env, fs};
 
 use r5d::search_reminders;
 
+/* Program configuration */
+struct Config {
+    files: Vec<String>, // files that should be processed
+}
+
+impl Config {
+    fn new() -> Config {
+        Config { files: Vec::new() }
+    }
+}
+
 fn main() {
-    let mut args = env::args_os();
-    args.next(); // Ignore program name
-    let filenames: Vec<_> = args
-        .map(|x| x.to_str().expect("argument parsing error").to_string())
-        .collect();
+    let mut config = Config::new();
+    parse_arguments(&mut config);
 
     let mut total_matches = 0;
+    let filenames = config.files;
 
-    for filename in filenames.clone() {
+    for filename in filenames.iter() {
         let matches = process(&filename);
 
         eprintln!("{filename} - {matches} match(es)");
@@ -54,4 +63,41 @@ fn process(filename: &str) -> i32 {
     }
 
     return rings;
+}
+
+fn usage() {
+    let progname = env::args_os().next().unwrap().to_str().unwrap().to_string();
+
+    println!("r5d - remind");
+    println!("  search for '!remind' statements in files and prints due reminders.");
+    println!("");
+    println!("Usage: {progname} FILES...");
+    println!("");
+    println!("");
+}
+
+fn parse_arguments(config: &mut Config) {
+    let mut args = env::args_os();
+    args.next(); // Ignore program name
+
+    for arg in args {
+        let arg = arg.to_str().expect("argument parsing error").to_string();
+        if arg == "" {
+            continue;
+        } else if arg.starts_with('-') {
+            // Program argument
+            match arg.as_str() {
+                "--help" | "-h" => {
+                    usage();
+                    std::process::exit(0);
+                }
+                _ => {
+                    eprintln!("invalid argument: {arg}");
+                    std::process::exit(1);
+                }
+            }
+        } else {
+            config.files.push(arg);
+        }
+    }
 }
